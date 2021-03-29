24fc9b13d9995ecfd69d654f85c9fbeafbb1b17a
zhanlianguf <zhanliang1994@gmail.com>
2021-03-29 12:43:05 -0400

    Add context and logs in server.go

--- api/server.go
+++ api/server.go
@@ -1,14 +1,18 @@
 package api // import "github.com/ZhanLiangUF/go-api-skeleton/api"
 
 import (
+	"context"
+	"crypto/tls"
 	"net"
 	"net/http"
+	"strings"
 
 	"github.com/ZhanLiangUF/go-api-skeleton/api/httputils"
 	"github.com/ZhanLiangUF/go-api-skeleton/api/middleware"
 	router "github.com/ZhanLiangUF/go-api-skeleton/api/router"
 	"github.com/ZhanLiangUF/go-api-skeleton/errdefs"
 	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
 )
 
 // can access version using mux.vars
@@ -16,9 +20,11 @@ const versionMatcher = "/v{version:[0-9.]+}"
 
 // Config provides configuration for server
 type Config struct {
-	Logging    bool
-	CorsHeader string
-	Version    string
+	Logging     bool
+	CorsHeader  string
+	Version     string
+	SocketGroup string
+	TLSConfig   *tls.Config
 }
 
 // Server contains instance details of the server
@@ -55,15 +61,22 @@ func (s *Server) Accept(addr string, listener net.Listener) {
 // Close closes server and stops receiving requests
 func (s *Server) Close() {
 	if err := s.srv.Close(); err != nil {
-		// log here?
+		logrus.Error(err)
 	}
 }
 
 // ServeAPI initialize servers
 func (s *Server) ServeAPI() error {
+	logrus.Infof("API listen on %s", s.srv.l.Addr())
 	s.srv.srv.Handler = s.createMux()
-	err := s.srv.Serve()
-	return err
+	var err error
+	if err := s.srv.Serve(); err != nil && strings.Contains(err.Error(), "use of closed network connection") {
+		err = nil
+	}
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // HTTPServer contains an instance of http server and listener
@@ -86,21 +99,27 @@ func (s *HTTPServer) Close() error {
 
 func (s *Server) makeHTTPHandler(handler httputils.APIFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		context := context.WithValue(r.Context(), UAStringKey{}, r.Header.Get("User-Agent"))
+		r.WithContext(context)
+
 		handlerFunc := s.handlerWithGlobalMiddlewares(handler)
 		vars := mux.Vars(r)
 		if vars == nil {
 			vars = make(map[string]string)
 		}
 		// How to use context? And what is it for?
-		if err := handlerFunc(ctx, w, r, vars); err != nil {
+		if err := handlerFunc(context, w, r, vars); err != nil {
 			statusCode := errdefs.GetHTTPErrorStatusCode(err)
 			if statusCode >= 500 {
-				// log
+				logrus.Errorf("Handler for %s %s returned error: %v", r.Method, r.URL.Path, err)
 			}
 		}
 
 	}
 }
+
+// UAStringKey is used as key type for user-agent string in net/context struct
+type UAStringKey struct{}
 
 // InitRouters initializes the list of routers for the server.
 func (s *Server) InitRouters(routers ...router.Router) {
@@ -118,10 +137,12 @@ func (pageNotFoundError) NotFound() {}
 // createMux initializes the main router
 func (s *Server) createMux() *mux.Router {
 	m := mux.NewRouter()
-	// log here?
+	logrus.Debug("Registering routes")
 	for _, apiRouter := range s.routers {
 		for _, r := range apiRouter.Routes() {
 			f := s.makeHTTPHandler(r.Handler())
+
+			logrus.Debugf("Registering %s, %s", r.Method(), r.Path())
 			m.Path(versionMatcher + r.Path()).Methods(r.Method()).Handler(f)
 			m.Path(r.Path()).Methods(r.Method()).Handler(f)
 		}
---
+++ apiversion/useragent.go
@@ -0,0 +1,4 @@
+package apiversion // import "github.com/ZhanLiangUF/go-api-skeleton/apiversion"
+
+// UAStringKey is used as key type for user-agent string in net/context struct
+type UAStringKey struct{}
