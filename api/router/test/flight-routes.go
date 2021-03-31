3d4fc0af7b40294d255d143b53d9eb41161b8d1e
zhanlianguf <zhanliang1994@gmail.com>
2021-03-31 19:42:26 -0400

    Start setting up server configuration

--- api/router/test/flight-routes.go
+++ api/router/test/flight-routes.go
@@ -7,6 +7,5 @@ import (
 
 func (s *testRouter) getFlightsGet(ctx context.Context, w http.ResponseWriter, r *http.Request, vars map[string]string) error {
 
-	w.Header().Set("Content-Type", "application/x-tar")
 	return nil
 }
--- cmd/test/main.go
+++ cmd/test/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/ZhanLiangUF/go-api-skeleton/api"
 	router "github.com/ZhanLiangUF/go-api-skeleton/api/router"
 	"github.com/ZhanLiangUF/go-api-skeleton/api/router/test"
 	"github.com/ZhanLiangUF/go-api-skeleton/pkg/jsonmessage"
@@ -11,6 +12,7 @@ import (
 )
 
 func main() {
+	// Set up logging
 	logrus.Info("Set up logger")
 	logrus.SetFormatter(&logrus.TextFormatter{
 		TimestampFormat: jsonmessage.RFC3339NanoFixed,
@@ -20,11 +22,14 @@ func main() {
 	// Create the log file if doesn't exist. And append to it if it already exists.
 	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
 	if err != nil {
-		// Cannot open log file. Logging to stderr
 		logrus.Error(err)
 	} else {
 		log.SetOutput(f)
 	}
+
+	serverConfig := &api.Config{}
+
+	// if tls?
 
 	routers := []router.Router{
 		test.NewRouter(),
